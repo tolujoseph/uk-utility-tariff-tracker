308eaac39173b727dd44d5390de4cf9afeb1dfd5
Tolu Ajayi <114291304+tolujoseph@users.noreply.github.com>
2025-05-16 20:36:21 +0100

    Remove venv from repo and update .gitignore

--- venv/tests/test_analyzer.py
+++
@@ -1,9 +0,0 @@
-from tracker.analyzer import compare_tariffs
-
-def test_compare_tariffs():
-    tariffs = [
-        {"provider": "A", "name": "Test1", "unit_rate": "15p/kWh", "standing_charge": "20p"},
-        {"provider": "B", "name": "Test2", "unit_rate": "14p/kWh", "standing_charge": "22p"},
-    ]
-    cheapest = compare_tariffs(tariffs)
-    assert cheapest["provider"] == "B"
--- venv/tests/test_scraper.py
+++
@@ -1,9 +0,0 @@
-from tracker.scraper import get_edf_tariffs, get_britishgas_tariffs
-
-def test_edf_scraper():
-    data = get_edf_tariffs()
-    assert isinstance(data, list)
-
-def test_bg_scraper():
-    data = get_britishgas_tariffs()
-    assert isinstance(data, list)
